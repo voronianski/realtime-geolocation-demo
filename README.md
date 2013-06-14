55c03315a99b496a92b766df071db9429c68d1c9
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-06-14 18:35:17 +0300

    remove node_modules folder, update dependencies, fix node-static for 0.10, readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-realtime-geolocation-demo
-=========================
+# realtime-geolocation-demo
+---
 
 Realtime geolocation app build on Node.js with HTML5 Geolocation API, Socket.io and Leaflet maps library.
 Tutorial and how to start with it you can read at http://tympanus.net/codrops/2012/10/11/real-time-geolocation-service-with-node-js/
+
+If you decided to clone this repo, don't forget to run inside it:
+
+```bash
+npm install
+```
+
+---
+
+(c) 2012 - 2013 Dmitri Voronianski
+
+**MIT Licensed**
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 var http = require('http');
-var static = require('node-static');
+var Static = require('node-static');
 var app = http.createServer(handler);
 var io = require('socket.io').listen(app);
 var port = 8080;
 
-var files = new static.Server('./public');
+var files = new Static.Server('./public');
 
-function handler(request, response) {
-	request.addListener('end', function() {
+function handler (request, response) {
+	request.on('end', function() {
 		files.serve(request, response);
-	});
+	}).resume();
 }
 
 // delete to see more logs from sockets
 io.set('log level', 1);
 
 io.sockets.on('connection', function (socket) {
-  socket.on('send:coords', function (data) {
-  	socket.broadcast.emit('load:coords', data);
-  });
+
+	socket.on('send:coords', function (data) {
+		socket.broadcast.emit('load:coords', data);
+	});
 });
 
 // start app on specified port
